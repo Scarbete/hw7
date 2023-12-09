8e315f02dd37ccefb0b1a4260a81f1c1e24502af
Scarbete <megavertal@gmail.com>
2023-12-09 14:38:39 +0600

    com

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import classes from './footer.module.sass'
+
+
+
+export const Footer = () => {
+    return (
+        <div className={classes.Footer}>
+            <div className={'container'}>
+                Footer
+            </div>
+        </div>
+    )
+}
